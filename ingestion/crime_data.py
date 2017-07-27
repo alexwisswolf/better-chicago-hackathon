ed6184addf877ffe87e5e63d9d3f2f0860967b9c
Alex Wiss-Wolferding <alexwisswolf@gmail.com>
2017-07-27 17:18:47 -0500

    Fixing crime data ingestion.

--- ingestion/crime_data.py
+++ ingestion/crime_data.py
@@ -12,7 +12,7 @@ for crime in crime_data:
     ds_crime = datastore.Entity(ds_client.key('crime', crime['case_number']))
     for key, val in crime.iteritems():
         if type(val) is dict:
-            ds_crime[key] = datastore.Entity(key=ds_client.key(key))
+            ds_crime[key] = datastore.Entity(ds_client.key(key))
             for nest_key, nest_val in val.iteritems():
                 ds_crime[key][nest_key] = nest_val
         else:
